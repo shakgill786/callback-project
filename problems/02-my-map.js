8788707d8c5f77fe794cc67e29ecc6ab83f35997
SunTheCoder <sunenglishjr@gmail.com>
2024-04-01 19:19:31 -0400

    problem two

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -17,8 +17,22 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
 function myMap(array, cb) {
-    // Your code here 
+    let arr = [];
+    for (let num of array) {
+
+        arr.push(cb(num))
+
+    }
+    return arr
 }
+
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
+
+let result2 = myMap(['run', 'Forrest'], function (el) {
+    return el.toUpperCase() + '!';
+});
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
