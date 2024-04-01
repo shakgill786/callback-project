e0e42f3cd526bbf2b40b1645dcafbbdcc0fb9e49
shakgill786 <sgillani1@gmail.com>
2024-04-01 19:08:47 -0400

    first problem

--- problems/01-my-for-each.js
+++ problems/01-my-for-each.js
@@ -22,8 +22,20 @@ console.log(test); // ['LAIKA', 'BELKA']
 *******************************************************************************/
 
 function myForEach(array, cb) {
-    // Your code here 
+    for (let i=0; i < array.length; i++){
+        cb(array[i], i, array);
+    }
 }
+
+myForEach(['a', 'b', 'c'], function (el, i) {
+    console.log (el + 'is at index ' + i);
+});
+
+let test = [];
+myForEach(['laika', 'belka'], function (el) {
+    test.push(el.toUpperCase());
+});
+console.log(test);
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
