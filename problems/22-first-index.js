0f0eda99cc7a6325c29924ec9e6564a1df576f86
shakgill786 <sgillani1@gmail.com>
2024-04-02 18:50:19 -0400

    problem 22

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -23,7 +23,14 @@ console.log(result3); // -1
 *******************************************************************************/
 
 function firstIndex(array, cb) {
-  // Your code here 
+    let resIndex = -1;
+
+    array.forEach((el, i) => {
+      if (cb(el) && resIndex === -1){
+        resIndex = i;
+      }
+    });
+    return resIndex;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
